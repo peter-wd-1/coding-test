7e248e849c039238c7d85f7cf9281b8efba5f0ba
peterismic@gmail.com <peterismic@gmail.com>
2021-11-30 11:40:06 -0500

    nope, overflow

--- src/components/Header/Atoms/HeaderSection.js
+++ src/components/Header/Atoms/HeaderSection.js
@@ -2,8 +2,6 @@ import styled from "@emotion/styled";
 import { Section } from "components/common";
 
 export const HeaderSection = styled(Section)`
-  width: 100vw;
-  overflow: hidden;
   z-index: 99;
   position: sticky;
   top: 0;
